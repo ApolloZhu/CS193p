bc96f145d420bdaf61e17843f3914ea6c99f2606
Apollo Zhu <public-apollonian@outlook.com>
2016-04-21 23:23:54 -0400

    Initial + Lesson 1 of 2016
    Stanford CS193p Course Files Since 2016 Spring

---
+++ 2016 Spring - iOS 9/Lesson 1/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Apollonian on 4/21/16.
+//  Copyright © 2016 WWITDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTyping = false
+
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    @IBAction func performOperation(sender: UIButton) {
+        if let methematicalSymbol = sender.currentTitle{
+            if methematicalSymbol == "π"{
+                display.text = String(M_PI)
+            }
+        }
+    }
+}
